65474e34e20682817b05dbdd9bd805e4ce821739
Mauri <mauricio.caricari@gmail.com>
2025-09-23 00:01:51 -0300

    Añadido de clases de reproducción de audio y de inyección de dependencias de las mismas

--- app/src/main/java/com/android/harmoniatpi/di/AudioModule.kt
+++ app/src/main/java/com/android/harmoniatpi/di/AudioModule.kt
@@ -1,8 +1,12 @@
 package com.android.harmoniatpi.di
 
+import com.android.harmoniatpi.data.audio.player.AudioPlayer
+import com.android.harmoniatpi.data.audio.player.AudioPlayerRepositoryImpl
+import com.android.harmoniatpi.data.audio.player.PcmAudioPlayer
 import com.android.harmoniatpi.data.audio.record.AudioRecorder
 import com.android.harmoniatpi.data.audio.record.AudioRecorderRepositoryImpl
 import com.android.harmoniatpi.data.audio.record.PcmAudioRecorder
+import com.android.harmoniatpi.domain.interfaces.AudioPlayerRepository
 import com.android.harmoniatpi.domain.interfaces.AudioRecorderRepository
 import dagger.Binds
 import dagger.Module
@@ -19,4 +23,11 @@ abstract class AudioModule {
 
     @Binds
     abstract fun bindAudioRecorder(impl: PcmAudioRecorder): AudioRecorder
+
+    @Binds
+    @Singleton
+    abstract fun bindAudioPlayerRepository(impl: AudioPlayerRepositoryImpl): AudioPlayerRepository
+
+    @Binds
+    abstract fun bindAudioRecorder(impl: PcmAudioPlayer): AudioPlayer
 }
