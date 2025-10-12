8e8b0c6614f36cac7f5c6ebe156a12016ea8bed2
Facu.T <111435243+TiernoFacundo@users.noreply.github.com>
2025-10-11 23:39:46 -0300

    Revert "Feature/project screen"

--- app/src/main/java/com/android/harmoniatpi/domain/model/project/Project.kt
+++
@@ -1,8 +0,0 @@
-package com.android.harmoniatpi.domain.model.project
-
-data class Project(
-    val title: String,
-    val description: String,
-    val hashtags: List<String>,
-    val audioWaveform: List<Float> = emptyList() // simulado o vacío
-)
