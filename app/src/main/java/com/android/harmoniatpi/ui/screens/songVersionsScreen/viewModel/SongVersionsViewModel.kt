d3343628a84435cb079cf24fbb07508beff20b7d
Federico Navarro <navarrofederico14@gmail.com>
2025-10-16 07:32:46 -0300

    feat: Integra el caso de uso para obtener detalles de la canción
    Se inyecta `GetSongDetailsUseCase` en `SongVersionsViewModel` para permitir la obtención de los detalles de una canción.
    
    Se crea el caso de uso `GetSongDetailsUseCase`, que actúa como intermediario con el `SongRepository` para recuperar la información detallada de una canción a través de su ID.

--- app/src/main/java/com/android/harmoniatpi/ui/screens/songVersionsScreen/viewModel/SongVersionsViewModel.kt
+++ app/src/main/java/com/android/harmoniatpi/ui/screens/songVersionsScreen/viewModel/SongVersionsViewModel.kt
@@ -6,6 +6,7 @@ import com.android.harmoniatpi.domain.model.song.DerivedVersion
 import com.android.harmoniatpi.domain.model.song.Song
 import com.android.harmoniatpi.domain.model.song.VersionType
 import com.android.harmoniatpi.domain.model.user.User
+import com.android.harmoniatpi.domain.usecases.GetSongDetailsUseCase
 import com.android.harmoniatpi.ui.screens.songVersionsScreen.model.SongVersionsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
@@ -17,7 +18,9 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SongVersionsViewModel @Inject constructor() : ViewModel() {
+class SongVersionsViewModel @Inject constructor(
+    private val getSongDetailsUseCase: GetSongDetailsUseCase
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(SongVersionsUiState())
     val uiState: StateFlow<SongVersionsUiState> = _uiState.asStateFlow()
@@ -38,7 +41,7 @@ class SongVersionsViewModel @Inject constructor() : ViewModel() {
             val originalSong = Song(
                 id = "original-01",
                 title = "El paso del tiempo",
-                creator = User(id = "creator-01", name = "Luna Beats", avatarUrl = null ),
+                creator = User(id = "creator-01", name = "Luna Beats", avatarUrl = null),
                 imageUrl = "url_de_imagen",
                 durationMillis = 464000L,
                 versionType = VersionType.ORIGINAL,
@@ -47,23 +50,79 @@ class SongVersionsViewModel @Inject constructor() : ViewModel() {
             )
 
             val derivedVersions = listOf(
-                DerivedVersion("v1",
-                    User(
+                DerivedVersion(
+                    "v1", User(
                         "u1",
                         "Luna Beats",
                         "https://images.unsplash.com/photo-1492684223066-81342ee5ff30"
-                    ), "projectA"),
-                DerivedVersion("v2", User("u2", "Echo Rivera", "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91"), "projectB"),
-                DerivedVersion("v3", User("u3", "Kai Harmonix", "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e"), "projectC"),
-                DerivedVersion("v4", User("u4", "Selene Nova", "https://images.unsplash.com/photo-1524504388940-b1c1722653e1"), "projectD"),
-                DerivedVersion("v5", User("u5", "Aria Flow", "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61"), "projectE"),
-                DerivedVersion("v6", User("u6", "Noah Frequenza", "https://images.unsplash.com/photo-1494790108377-be9c29b29330"), "projectF"),
-                DerivedVersion("v7", User("u7", "Zion Wave", "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d"), "projectG"),
-                DerivedVersion("v8", User("u8", "Vera Pulse", "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e"), "projectH"),
-                DerivedVersion("v9", User("u9", "Milo Resonance", "https://images.unsplash.com/photo-1521119989659-a83eee488004"), "projectI"),
-                DerivedVersion("v10", User("u10", "Nia Groove", "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde"), "projectJ"),
-                DerivedVersion("v11", User("u11", "Riley Sound", "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde"), "projectK"),
-                DerivedVersion("v12", User("u12", "Ivy Echo", "https://images.unsplash.com/photo-1544005313-94ddf0286df2"), "projectL")
+                    ), "projectA"
+                ), DerivedVersion(
+                    "v2", User(
+                        "u2",
+                        "Echo Rivera",
+                        "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91"
+                    ), "projectB"
+                ), DerivedVersion(
+                    "v3", User(
+                        "u3",
+                        "Kai Harmonix",
+                        "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e"
+                    ), "projectC"
+                ), DerivedVersion(
+                    "v4", User(
+                        "u4",
+                        "Selene Nova",
+                        "https://images.unsplash.com/photo-1524504388940-b1c1722653e1"
+                    ), "projectD"
+                ), DerivedVersion(
+                    "v5", User(
+                        "u5",
+                        "Aria Flow",
+                        "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61"
+                    ), "projectE"
+                ), DerivedVersion(
+                    "v6", User(
+                        "u6",
+                        "Noah Frequenza",
+                        "https://images.unsplash.com/photo-1494790108377-be9c29b29330"
+                    ), "projectF"
+                ), DerivedVersion(
+                    "v7", User(
+                        "u7",
+                        "Zion Wave",
+                        "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d"
+                    ), "projectG"
+                ), DerivedVersion(
+                    "v8", User(
+                        "u8",
+                        "Vera Pulse",
+                        "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e"
+                    ), "projectH"
+                ), DerivedVersion(
+                    "v9", User(
+                        "u9",
+                        "Milo Resonance",
+                        "https://images.unsplash.com/photo-1521119989659-a83eee488004"
+                    ), "projectI"
+                ), DerivedVersion(
+                    "v10", User(
+                        "u10",
+                        "Nia Groove",
+                        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde"
+                    ), "projectJ"
+                ), DerivedVersion(
+                    "v11", User(
+                        "u11",
+                        "Riley Sound",
+                        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde"
+                    ), "projectK"
+                ), DerivedVersion(
+                    "v12", User(
+                        "u12",
+                        "Ivy Echo",
+                        "https://images.unsplash.com/photo-1544005313-94ddf0286df2"
+                    ), "projectL"
+                )
             )
 
             // Actualizamos el estado con los datos cargados.
@@ -109,4 +168,6 @@ class SongVersionsViewModel @Inject constructor() : ViewModel() {
         if (projectId == null) return
         // TODO: Lógica para navegar a la pantalla del proyecto
     }
+
+
 }
