c460024c9e0556f74a5eeb0c58dcff06f22a7575
BarzabalC <cristian.barzabal@ocasa.com>
2025-10-22 14:09:20 -0300

    Refactor UI v3

--- app/src/main/java/com/android/harmoniatpi/ui/screens/menuPrincipal/content/optionsScreens/myPostScreen/MyPostsScreen.kt
+++ app/src/main/java/com/android/harmoniatpi/ui/screens/menuPrincipal/content/optionsScreens/myPostScreen/MyPostsScreen.kt
@@ -1,45 +1,23 @@
 package com.android.harmoniatpi.ui.screens.menuPrincipal.content.optionsScreens.myPostScreen
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.outlined.Comment
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -50,14 +28,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import com.android.harmoniatpi.domain.model.userPreferences.Post
-import com.android.harmoniatpi.ui.screens.homeScreen.tabs.communityScreen.components.CommentItem
 import com.android.harmoniatpi.ui.screens.homeScreen.tabs.communityScreen.components.CommentsBottomSheetContent
 import com.android.harmoniatpi.ui.screens.homeScreen.tabs.communityScreen.components.PostCard
 import com.android.harmoniatpi.ui.screens.menuPrincipal.content.model.OptionsMenu
@@ -88,7 +61,9 @@ fun MyPostsScreen(
             CenterAlignedTopAppBar(
                 title = {
                     Text(
-                        modifier = Modifier.fillMaxWidth().padding(end = 16.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(end = 16.dp),
                         text = "Mis publicaciones",
                         style = MaterialTheme.typography.titleLarge,
                         textAlign = TextAlign.Center
@@ -111,7 +86,6 @@ fun MyPostsScreen(
             )
         },
         // --- CAMBIO 3: Fondo consistente con CommunityScreen ---
-        containerColor = MaterialTheme.colorScheme.background,
         containerColor = MaterialTheme.colorScheme.background,
     ) { innerPadding ->
         Box(
